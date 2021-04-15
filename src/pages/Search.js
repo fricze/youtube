d6de03cdd565ecbaa7e8b0f9906fcbc69b1fe555
jonki <andrzej.fricze@gmail.com>
2021-04-15 13:36:50 +0200

    Stage 0

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,26 +1,10 @@
 import unescape from "unescape";
-import { List } from "../components/List";
-import { Paging } from "../components/Paging";
-import { useSearch } from "../services/yt";
 
 const Info = ({ infoText }) => <div className="search-info">{infoText}</div>;
 
-export const Videos = ({ query, pageToken }) => {
-  const videos = useSearch({ query, pageToken });
-  return (
-    <div>
-      <Info infoText={"Videos for '" + unescape(query) + "'"} />
-
-      <Paging path={"/search/" + query + "/"} {...videos} />
-      <List {...videos} />
-      <Paging path={"/search/" + query + "/"} {...videos} />
-    </div>
-  );
-};
-
 export const Search = ({ pageToken, query, infoText }) =>
   query ? (
-    <Videos query={query} pageToken={pageToken} />
+    <Info infoText={"Videos for '" + unescape(query) + "'"} />
   ) : infoText ? (
     <Info infoText={infoText} />
   ) : (
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,26 +1,3 @@
-import unescape from "unescape";
-import { useVideo } from "../services/yt";
-import { Tags } from "../components/Tags";
-import { SingleVideo } from "../components/SingleVideo";
-
 export const Video = ({ id }) => {
-  const video = useVideo(id);
-
-  if (!video) {
-    return <div>loading</div>;
-  }
-
-  const { tags, channelId, channelTitle } = video.snippet;
-
-  return (
-    <article className="video-page">
-      <SingleVideo id={id} />
-
-      <a href={`https://www.youtube.com/channel/${channelId}`}>
-        <h2 className="video-channel">channel: {unescape(channelTitle)}</h2>
-      </a>
-
-      <Tags tags={tags} />
-    </article>
-  );
+  return <article className="video-page">Single video</article>;
 };
